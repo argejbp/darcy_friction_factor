f9b259b014927a748651e1d596a0c2a35e99501c
Argenis <45941131+argejbp@users.noreply.github.com>
2021-09-23 07:49:19 -0400

    Update Main_Friction_File.cpp

--- Main_Friction_File.cpp
+++ Main_Friction_File.cpp
@@ -1,6 +1,3 @@
-//Written by Argenis Bonilla, updated in 01/2021
-//aj.bonilla.13@gmail.com
-
 #include <iostream>
 #include <cmath>
 #include "Friction_Functions.h"
@@ -9,22 +6,35 @@ using namespace std;
 int main(){
 	
 	double Re, f, D;
-	int g, p;
+	int g, p, a = 1;
 	cout << "Welcome to the interactive program to \ncalculate the Darcy-Weisbach friction factor in pipe flow \n\n";
 	cout << "First, the flow regime must be known \n\n";
-	cout << "Enter 1 or 2 according the known variable: \n\n";
-	cout << "Volumetric Flowrate [1]" << endl;
-	cout << "Fluid velocity [2]" << endl;
-	cout << "Your choice: ";
-	cin >> g;
 	
-	cout <<"\nEnter the pipe inner diameter in [mm]: ";
-	cin >> D;
-	Re = Reynolds(g, D);
-	f = friction_factor(Re, D);
-	print_values(Re, f);
-
-	cout << "\n\nGood Bye, see you next time" << endl;
+	do
+	{
+		cout << "Enter 1 or 2 according the known variable: \n\n";
+		cout << "Volumetric Flowrate [1]" << endl;
+		cout << "Fluid velocity [2]" << endl;
+		cout << "Your choice: ";
+		cin >> g;
+		
+		cout <<"\nEnter the pipe inner diameter in [mm]: ";
+		cin >> D;
+		Re = Reynolds(g, D);
+		f = friction_factor(Re, D);
+		print_values(Re, f);
+		
+		cout << "\n\nWould you like to keep doing calculations?";
+		cout << "\n[0] No ";
+		cout << "\n[1] Yes ";
+		cout << "\nYour choice: ";
+		cin >> a;
+		cout << "\n";
+	
+			
+	} while(a!=0);
+	
+	cout << "\nGood Bye, see you next time" << endl;
 	system("pause");
 	return 0;
 }
